3a3c6fc4bc8614a1ee08728a75bc4e57263b109c
David Erlich <43164727+daviderl99@users.noreply.github.com>
2023-02-11 02:39:48 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <button class="button" v-if="!showModal" @click="showModal=true">
+      Nupp 1
+    </button>
+    <ModalItem v-if="showModal" @submit="submitValues" @close="showModal=false" :show="showModal" />
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.button {
+  background-color: inherit;
+  color: white;
+  padding: 10px 20px;
+  border: 1px solid var(--color-border);
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--vt-c-black-mute);
+  }
+}
+</style>
+
+<script lang="ts">
+import ModalItem from './components/ModalItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ModalItem,
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    submitValues(values: { valueA: any; valueB: any; }) {
+      console.log('A:', values.valueA);
+      console.log('B:', values.valueB);
+    },
+  },
+};
+</script>
---
+++ src/components/ModalItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="modal" v-if="show">
+    <h1>Modaal</h1>
+    <div class="inputs">
+      <input type="text" v-model="valueA" placeholder="A">
+      <input type="text" v-model="valueB" placeholder="B">
+    </div>
+    <div class="buttons">
+      <button @click="submitValues">Sisesta</button>
+      <button @click="closeModal">Sulge</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.modal {
+  color: white;
+  padding: 20px;
+  border: 1px solid var(--color-border);
+  border-radius: 5px;
+}
+.inputs {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+
+  & > input {
+    background-color: inherit;
+    color: white;
+    margin: 5px 0;
+    padding: 10px;
+    border: 1px solid var(--color-border);
+    border-radius: 5px;
+  }
+}
+.buttons {
+  display: flex;
+  justify-content: space-between;
+
+  & > button {
+    background-color: inherit;
+    color: white;
+    padding: 10px 20px;
+    border: 1px solid var(--color-border);
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: var(--vt-c-black-mute);
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+export default {
+  name: 'ModalItem',
+  props: {
+    show: Boolean,
+  },
+  data() {
+    return {
+      valueA: '',
+      valueB: '',
+    };
+  },
+  methods: {
+    submitValues() {
+      this.$emit('submit', { valueA: this.valueA, valueB: this.valueB });
+      this.closeModal();
+    },
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
